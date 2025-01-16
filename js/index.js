9dc7e6561aa8db8ed0db846c5766b1863d1519db
Syed Huzaif <syedhuzaif199@gmail.com>
2025-01-16 22:09:03 +0530

    Added Microsoft Copilot AI page

--- js/index.js
+++ js/index.js
@@ -8,3 +8,23 @@ fetch("/footer.html")
   .then((data) => {
     document.getElementById("footer").innerHTML = data;
   });
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll(".toggler").forEach((toggler) => {
+    toggler.addEventListener("click", function () {
+      toggler.querySelectorAll(".toggleable").forEach((toggleable) => {
+        toggleable.classList.toggle("hidden");
+      });
+      toggler
+        .querySelectorAll(".description__plus")
+        .forEach((description__plus) => {
+          description__plus.classList.toggle("hidden");
+        });
+      toggler
+        .querySelectorAll(".description__minus")
+        .forEach((description__minus) => {
+          description__minus.classList.toggle("hidden");
+        });
+    });
+  });
+});
